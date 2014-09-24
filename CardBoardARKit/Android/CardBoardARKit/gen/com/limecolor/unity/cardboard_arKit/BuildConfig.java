d98e9f4d6c14321965557c6c1bddb90ae9592319
flashscope <flashscope@naver.com>
2014-09-25 01:02:19 +0900

    Add AR Project Android project

---
+++ CardBoardARKit/Android/CardBoardARKit/gen/com/limecolor/unity/cardboard_arKit/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.limecolor.unity.cardboard_arKit;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CardBoardARKit/CardBoardARKit/Assets/Scripts/TriggerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerInput : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject dummyGunObject = null;
+	
+	[SerializeField]
+	private GameObject bulletObject = null;
+	
+	void Update()
+	{	
+		if ( Input.GetButtonDown("Fire1") )
+		{
+			Shot();
+		}
+	}
+
+	void GetTriggerEvent()
+	{
+		Shot();
+	}
+
+	private void Shot()
+	{
+		// nothing in here now...
+	}
+
+}
